c4776bd23c49ba58d2ad4477bd48f831c4020dd9
Filip Kwasik <fkwasik@interia.pl>
2019-04-27 14:55:10 +0200

    Updates

---
+++ precache-manifest.6f968c386fc1fd5ef00e37bde7ec7858.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "00956046e8a4da55c75acb944111582d",
+    "url": "/weather-react-app/static/media/Rain-movie.00956046.mp4"
+  },
+  {
+    "revision": "4fb2c665b93578c6214c",
+    "url": "/weather-react-app/static/css/main.45508c4f.chunk.css"
+  },
+  {
+    "revision": "7f73905e939fe825089b",
+    "url": "/weather-react-app/static/js/runtime~main.608e19b5.js"
+  },
+  {
+    "revision": "7af1bcf1a350315318b3",
+    "url": "/weather-react-app/static/js/2.2febcc6e.chunk.js"
+  },
+  {
+    "revision": "8d79ee1d982a449d5dda4a85072541fe",
+    "url": "/weather-react-app/static/media/Clouds-movie.8d79ee1d.mp4"
+  },
+  {
+    "revision": "4fb2c665b93578c6214c",
+    "url": "/weather-react-app/static/js/main.6e15666f.chunk.js"
+  },
+  {
+    "revision": "29b9c4991de86e4211d2129863b9d315",
+    "url": "/weather-react-app/static/media/Clear-movie.29b9c499.mp4"
+  },
+  {
+    "revision": "dad8d921df88f90d374606cb40dedbc2",
+    "url": "/weather-react-app/static/media/Drizzle-movie.dad8d921.mp4"
+  },
+  {
+    "revision": "12fece265e2aaa498e9faaba351e41db",
+    "url": "/weather-react-app/static/media/Thunderstorm-movie.12fece26.mp4"
+  },
+  {
+    "revision": "c8e390e048fad8726eacedcc47b0482d",
+    "url": "/weather-react-app/static/media/Main-movie.c8e390e0.mp4"
+  },
+  {
+    "revision": "081d35b309e94657568189865870bdf0",
+    "url": "/weather-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Bankot/weather-react-app/precache-manifest.77245d31807341c96e27f950dc105e1d.js"
+  "/weather-react-app/precache-manifest.6f968c386fc1fd5ef00e37bde7ec7858.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Bankot/weather-react-app/index.html", {
+workbox.routing.registerNavigationRoute("/weather-react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
